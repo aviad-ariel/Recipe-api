cf9c01b3e4c79325532800240ef72e42d7345290
aviad <arielaviad1@gmail.com>
2020-02-07 18:16:37 +0200

    Added custom user model to admin site

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,27 +1,27 @@
- from django.test import TestCase, Client
- from django.contrib.auth import get_user_model
- from django.urls import reverse
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
- 
- class AdminSiteTests(TestCase):
 
-     def setup(self):
-         self.client = Client()
-         self.admin_user = get_user_model().objects.create_superuser(
-             email='test@admin.com',
-             password='admin123'
-         )
-         self.client.force_login(self.admin_user)
-         self.user = get_user_model().objects.create_user(
-             email='test@user.com',
-             password='user123',
-             name='Regular User'
-         )
+class AdminSiteTests(TestCase):
 
-         def test_user_listet(self):
-             """Test to check listed user on user page"""
-             url = reverse('admin:core_user_changelist')
-             res = self.client.get(url)
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='test@admin.com',
+            password='admin123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@user.com',
+            password='user123',
+            name='Regular User'
+        )
 
-             assertContains(res, self.user.name)
-             assertContains(res, self.user.email)
+    def test_user_listed(self):
+        """Test to check listed user on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
